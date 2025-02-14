3e66923ca8f27cbdb08b9be29045cc032409c575
Slimobedsama <ohwo.okoro@yahoo.com>
2025-02-14 02:23:50 +0100

    created grid cells and styles

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,14 @@ const mainContainer = document.getElementById('main-container');
 const gridContainer = document.createElement('div');
 gridContainer.setAttribute('id', 'grid-container');
 mainContainer.appendChild(gridContainer);
+
+function createGrid(userInput) {
+    const gridSize = parseInt(userInput);
+    for(i = 0; i < (gridSize * gridSize); i++) {
+        let cells = document.createElement('div');
+        cells.setAttribute('class', 'cell');
+        cells.style.flexBasis = 100 / gridSize + '%';
+        gridContainer.appendChild(cells);
+    }
+}
+createGrid()
