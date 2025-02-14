4bd299be1322c3ba7cb2fa1759c0c73294df4082
Slimobedsama <ohwo.okoro@yahoo.com>
2025-02-14 01:58:26 +0100

    creation of grid container in javascript and the addition of the container styles

---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+const mainContainer = document.getElementById('main-container');
+const gridContainer = document.createElement('div');
+gridContainer.setAttribute('id', 'grid-container');
+mainContainer.appendChild(gridContainer);
